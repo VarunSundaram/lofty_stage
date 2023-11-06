1372a506731699da45efbbab8baef4b7d5b1811d
VarunSundaram <varunksundaram@gmail.com>
2023-11-06 18:40:17 +0530

    something changed

--- function_app.py
+++ function_app.py
@@ -1,5 +1,7 @@
 import logging
 import azure.functions as func
+import requests
+import time
 
 app = func.FunctionApp()
 
@@ -10,3 +12,33 @@ def lofty_staging(myTimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function executed.')
+    stage_my_lofty()
+    
+def stage_my_lofty():
+    api = "https://management.azure.com/subscriptions/cf74786c-6831-4d2b-84c7-9a4e95d202dc/resourceGroups/bollingerposition/providers/Microsoft.Web/sites/lofty-az/functions/loftypts/properties/state?api-version=2018-11-01"
+    
+    data = {
+        "properties":"disabled"
+        }
+    
+    response = requests.post(f"{api}", json=data)
+    if response.status_code == 200:
+        logging.info('the lofty application is disabled for the comfort')
+    else:
+        logging.info('the lofty application is not disabled and this is worriesome')
+        return
+    time.sleep(30)
+    
+    data = {
+        "properties":"enabled"
+        }
+    
+    response = requests.post(f"{api}", json=data)
+    if response.status_code == 200:
+        logging.info('the lofty application is enabled for the comfort')
+    else:
+        logging.info('the lofty application is not enabled and this is worriesome')
+    return
+
+#if __name__ == "__main__":
+#    stage_my_lofty()
