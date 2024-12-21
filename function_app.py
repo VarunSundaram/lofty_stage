9ee52f8bb07b25066bfacbe56a5f67fe6091fec2
VarunSundaram <varunksundaram@gmail.com>
2024-12-21 13:25:53 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -13,13 +13,13 @@ import msal
 app = func.FunctionApp()
 
 # Tenant ID for your Azure Subscription
-TENANT_ID = '536391cd-606c-460c-b9c8-b394d89b7a63'
+TENANT_ID = 'XXXXXXXXXXXXXXXX'
 
 # Your Service Principal Sectret Password
-KEY = 'F4c8Q~mgmy33EZT.waROdQeoMBdIUdVPjjgwtbal'
+KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
 
 # Your Client ID or App Id
-APP_ID = "4e19fbe7-2e9a-402a-aebf-3b7b54a97aa6"
+APP_ID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 # Resource Name
 RESOURCE_GROUP = "bollingerposition"
@@ -28,7 +28,7 @@ RESOURCE_GROUP = "bollingerposition"
 APP_NAME = "lofty-az"
 
 # Subscription
-SUBSCRIPTION = "cf74786c-6831-4d2b-84c7-9a4e95d202dc"
+SUBSCRIPTION = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
 @app.schedule(schedule="* 0/30 * * * *", arg_name="myTimer", run_on_startup=True,
 #@app.schedule(schedule="* 0/5 * * * 1-5", arg_name="myTimer", run_on_startup=True,
@@ -49,7 +49,7 @@ def lofty_staging(myTimer: func.TimerRequest) -> None:
         raise Exception(e)
     
 def stage_my_lofty():
-    #token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IlQxU3QtZExUdnlXUmd4Ql82NzZ1OGtyWFMtSSIsImtpZCI6IlQxU3QtZExUdnlXUmd4Ql82NzZ1OGtyWFMtSSJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuY29yZS53aW5kb3dzLm5ldCIsImlzcyI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzUzNjM5MWNkLTYwNmMtNDYwYy1iOWM4LWIzOTRkODliN2E2My8iLCJpYXQiOjE3MDA1ODc0MTgsIm5iZiI6MTcwMDU4NzQxOCwiZXhwIjoxNzAwNTkxNjA5LCJhY3IiOiIxIiwiYWlvIjoiQVlRQWUvOFZBQUFBNTVJc216R080ZGpnbkI4ci9WaHZRVW1ta3pZbEJCdXRKOEpEbitjblFuMys3RUdwQmVlUDJ4QWdwUVpaYXR5dVU3VDJtNkpNbnBJbWZnU1Foa1JrRWFtRklnVUM1My8vL1NYS1JUZ1dOSW0xaTdZZEdwTCtrUUpSTVpsVGxab1RUK2NjY2M4bStZSURqU1FQTXV0N2k2MGl4d2ZORHN1SjY0WkNONzA1U2U4PSIsImFsdHNlY2lkIjoiMTpsaXZlLmNvbTowMDAzMDAwMEIxNDc1NzQyIiwiYW1yIjpbInB3ZCIsIm1mYSJdLCJhcHBpZCI6IjE4ZmJjYTE2LTIyMjQtNDVmNi04NWIwLWY3YmYyYjM5YjNmMyIsImFwcGlkYWNyIjoiMCIsImVtYWlsIjoidmFydW5zdW5kYXJhbUBvdXRsb29rLmNvbSIsImZhbWlseV9uYW1lIjoiU3VuZGFyYW0iLCJnaXZlbl9uYW1lIjoiVmFydW4ga3VtYXIiLCJncm91cHMiOlsiOWNhMTQwOTgtZGY3NC00MmE0LWFjZDYtMmMzMTg2ZGJlMGNjIl0sImlkcCI6ImxpdmUuY29tIiwiaWR0eXAiOiJ1c2VyIiwiaXBhZGRyIjoiMTYzLjExNi4yMTQuNjQiLCJuYW1lIjoiVmFydW4ga3VtYXIgU3VuZGFyYW0iLCJvaWQiOiIwNDA1MTQwOC02MjU5LTQxODgtOTQwNS0yOTYyMGE3N2FlNWQiLCJwdWlkIjoiMTAwMzIwMDJFRUMwRjQyNiIsInJoIjoiMC5BVDBBelpGalUyeGdERWE1eUxPVTJKdDZZMFpJZjNrQXV0ZFB1a1Bhd2ZqMk1CT2hBR1UuIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic3ViIjoidmdWbDNWVzlsSG5YR1hVNGxPTE9aS1NoVjAyWlFKekZDaTRQMG8tTHF3SSIsInRpZCI6IjUzNjM5MWNkLTYwNmMtNDYwYy1iOWM4LWIzOTRkODliN2E2MyIsInVuaXF1ZV9uYW1lIjoibGl2ZS5jb20jdmFydW5zdW5kYXJhbUBvdXRsb29rLmNvbSIsInV0aSI6IjVQU3VrdHRQcDB5RmhtMmR0Zmp3QUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbIjYyZTkwMzk0LTY5ZjUtNDIzNy05MTkwLTAxMjE3NzE0NWUxMCIsImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdLCJ4bXNfY2FlIjoiMSIsInhtc190Y2R0IjoxNjk0MzI2MTE2fQ.RVHZ3mk7kztgeV8GAcDvhshlQQ4hbMT5Qcf8r5Rii_G_YeNZR65oNNNbigMqbiQw0pzDLiAPneYzBa3ptPyZarfBOwE3RUtZX9inlfGz0knFRPD4NY4_ZffsmCxDzkAse9HQe_ZS7D5IqoLmgfcTbc9kAoyHhRmGaGUC2xIW-r4fs0ScVx5GzReAqnBKH9xraz9EuIAG24HEgp78FfAqjIC2Zab5kgjkNDHvx280DYrVk-BbcmpnEq6_ggrrz_VNN6YzPrsqN2jDiXfgud3nd6vCYYJBv7DlLv1mnkKy2woyxEaZ2jL_4SCJpGWTOVFEFoq_UAb2Nhnab1b66vB3iQ"
+    #token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     token = get_access_token()
         
     if "none" in token:
@@ -172,8 +172,8 @@ def get_access_token():
 def authenticate():
     TOKEN_URL = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token"
 
-    request_payload = {"username": "varunsundaram@outlook.com",
-                       "password": "Juliet#1",
+    request_payload = {"username": "XXXXXXXXXXXXXXXXXXX@XXXXXXXXXX",
+                       "password": "XXXXXXXXXXXX",
                        "scope": ".admin", #".default",
                        "grant_type": "client_credentials",
                        "client_id": APP_ID,
